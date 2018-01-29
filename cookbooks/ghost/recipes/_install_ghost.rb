92d91874b48fd0eb4c5fa042b439d706dc8e9897
Christopher Cureau <cmcureau@gmail.com>
2018-01-28 22:17:23 -0600

    now we put it all together...

--- cookbooks/ghost/recipes/_install_ghost.rb
+++ cookbooks/ghost/recipes/_install_ghost.rb
@@ -29,18 +29,21 @@ bash 'download and configure ghost' do
   cwd node['ghost']['homedir']
   code <<-EOH
     ghost install --no-stack --no-start --no-setup --db mysql --dbhost #{node['mariadb']['host']} --dbuser #{node['mariadb']['user']} --dbpass #{node['mariadb']['pass']} --dbname #{node['mariadb']['database']} --url http://127.0.0.1:2368
-  EOH
-end
-
-bash 'fix permissions' do
-  code <<-EOH
-    chown #{node['ghost']['user']}:#{node['ghost']['group']} #{node['ghost']['homedir']}/content
-  EOH
-end
-
-bash 'ghost database migrations' do
-  cwd node['ghost']['homedir']
-  code <<-EOH
+    chown -R #{node['ghost']['user']}:#{node['ghost']['group']} #{node['ghost']['homedir']}/content
+    ghost config --db mysql --dbhost #{node['mariadb']['host']} --dbuser #{node['mariadb']['user']} --dbpass #{node['mariadb']['pass']} --dbname #{node['mariadb']['database']} --url http://127.0.0.1:2368
     #{node['ghost']['homedir']}/current/node_modules/.bin/knex-migrator-migrate --init --mgpath #{node['ghost']['homedir']}/current
   EOH
 end
+
+# bash 'fix permissions' do
+#   code <<-EOH
+#     chown -R #{node['ghost']['user']}:#{node['ghost']['group']} #{node['ghost']['homedir']}/content
+#   EOH
+# end
+#
+# bash 'ghost database migrations' do
+#   cwd node['ghost']['homedir']
+#   code <<-EOH
+#     #{node['ghost']['homedir']}/current/node_modules/.bin/knex-migrator-migrate --init --mgpath #{node['ghost']['homedir']}/current
+#   EOH
+# end
